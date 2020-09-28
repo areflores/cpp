dda52d85965648d7e89f571e49a57e0440aa78cb
Areli Flores <floresareli1998@gmail.com>
2020-09-28 12:52:02 -0600

    agragando ejercicio2

--- L2/Ejercicio2-Biblioteca/Biblioteca.cpp
+++ L2/Ejercicio2-Biblioteca/Biblioteca.cpp
@@ -65,9 +65,9 @@ int  main ( int argc, char  const * argv [])
 
                 cout << " Tambien te sugerimos estos libros: " << endl;
 
-                int sugerencia1 = rand ()% 4 + 1 ;
-                int sugerencia2 = rand ()% 4 + 1 ;
-                int sugerencia3 = rand ()% 4 + 1 ;
+                int sugerencia1 = rand ()% 5 ;
+                int sugerencia2 = rand ()% 5 ;
+                int sugerencia3 = rand ()% 5 ;
 
                 cout << " Sugerencia 1: " << libros [sugerencia1] [ 1 ] << endl;
                 cout << " Sugerencia 2: " << libros [sugerencia2] [ 1 ] << endl;
